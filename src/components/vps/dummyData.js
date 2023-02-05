a95ce00847bbcf7d48d019ec8df5c0e34e140b19
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-02-05 22:26:23 +0530

    vps page complated

---
+++ src/components/vps/dummyData.js
@@ -0,0 +1,49 @@
+export const gridData = {
+    gridData1 : [
+        {
+          img:"https://www.btrackindia.com/assets/img/a.EachCloudVPS.png",
+          des:"Reliable Security and Disaster Recovery",
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/b.CloudVPSenables.png",
+          des:"Wider Competitive Reach Easy to Manage",
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/c.VPSCloudismoresecure.png",
+          des:"Increased Flexibility, Reduced Costs",
+        },
+        {
+            img:"https://www.btrackindia.com/assets/img/d.dedicated%20ip.png",
+            des:"Enhanced Customer Satisfaction Web Access",
+
+          }
+      ],
+    gridData2 : [
+        {
+          img:"https://www.btrackindia.com/assets/img/a.EachCloudVPS.png",
+          des:"24*7 support Call & Chats",
+          link:"/"
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/b.CloudVPSenables.png",
+          des:"Data Backup ",
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/c.VPSCloudismoresecure.png",
+          des:"Strong Expert Support",
+        },
+        {
+            img:"https://www.btrackindia.com/assets/img/d.dedicated%20ip.png",
+            des:"99.9% uptime",
+
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/a.EachCloudVPS.png",
+          des:"Help in Migration",
+        },
+        {
+          img:"https://www.btrackindia.com/assets/img/b.CloudVPSenables.png",
+          des:"Encryption data security",
+        },
+      ]
+}
